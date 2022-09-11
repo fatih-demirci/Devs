6c82cc4a9f6dc028280c9ad01066b3e0f8fac7ff
Fatih DEMİRCİ <fatih.demirci61@hotmail.com>
2022-09-12 01:45:22 +0300

    .

--- src/projects/kodlama.io/Presentation/Devs.WebAPI/Controllers/AuthController.cs
+++ src/projects/kodlama.io/Presentation/Devs.WebAPI/Controllers/AuthController.cs
@@ -1,15 +1,7 @@
-﻿using Core.Security.Entities;
-using Core.Security.Enums;
-using Core.Security.Hashing;
-using Core.Security.JWT;
-using Devs.Application.Features.Auth.Commands.RegisterDeveloper;
+﻿using Devs.Application.Features.Auth.Commands.RegisterDeveloper;
 using Devs.Application.Features.Auth.DTOs;
 using Devs.Application.Features.Auth.Queries.Login;
-using Devs.Application.Services.Repositories;
-using Devs.Domain.Entities;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Devs.WebAPI.Controllers
 {
@@ -17,19 +9,6 @@ namespace Devs.WebAPI.Controllers
     [ApiController]
     public class AuthController : BaseController
     {
-        IDeveloperRepository _developerRepository;
-        ITokenHelper _tokenHelper;
-        IUserOperationClaimRepository _userOperationClaimRepository;
-        IUserRepository _userRepository;
-
-        public AuthController(IDeveloperRepository developerRepository, ITokenHelper tokenHelper, IUserOperationClaimRepository userOperationClaimRepository, IUserRepository userRepository = null)
-        {
-            _developerRepository = developerRepository;
-            _tokenHelper = tokenHelper;
-            _userOperationClaimRepository = userOperationClaimRepository;
-            _userRepository = userRepository;
-        }
-
         [HttpPost]
         public async Task<IActionResult> RegisterDeveloper([FromBody] RegisterDeveloperCommandRequest registerDeveloperCommandRequest)
         {
